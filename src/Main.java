aeda4d6b7383baff7c61161dc599027e8193473b
euniece-kyle <ekpuerto89843@liceo.edu.ph>
2025-08-01 11:23:13 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Main Branch!");
+import java.util.Scanner;
+
+public class SumoftheNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int TotalNumber = 0;
+
+        for (int i = 1; i <= 5; i++){
+            System.out.println("Enter the number " + i + ":");
+            int number = scanner.nextInt();
+            TotalNumber += number;
+        }
+        System.out.println("Total Number: " + TotalNumber);
+
+        scanner.close();
     }
+
 }
