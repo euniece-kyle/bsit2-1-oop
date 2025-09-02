e3f85192fa3146d2c615eeb31ce68f1c645cf97a
euniece-kyle <ekpuerto89843@liceo.edu.ph>
2025-09-02 10:05:12 +0800

    Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        LibraryManager libraryManager = new LibraryManager();
+
+        libraryManager.showBooks();
+      
+        libraryManager.addBook();
+
+        libraryManager.removeBook();
+
+        System.out.println("\nProgram completed successfully.");
+    }
+}
